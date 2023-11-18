083b2b5403f47f61ec3b1932d5e1996592a856b5
burbiee <103138667+burbiee@users.noreply.github.com>
2023-11-18 11:39:29 +0100

    Update fer.pyx
    Added creator operator

--- fer.pyx
+++ fer.pyx
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 15 20:42:50 2023
+
+@author: burbiee
+"""
+
 # Imports
 
 cimport cython
@@ -27,8 +34,7 @@ ctypedef struct fstate:
     
 cpdef void print_fstate(fstate in_state):
     print(format(in_state.state, '0'+str(in_state.size)+'b'),"\n")
-
-
+    
 # Brian Kernighan's Algorithm (counting ones in a fstate)
 @cython.boundscheck(False)
 @cython.wraparound(False)
@@ -53,13 +59,13 @@ cdef int count_ones_spin(fstate in_fstate, int in_index) nogil:
 # @site is a number that starts from 1 and counts from the left
 # returns 0 r 1
 
-cdef inline int has_occupation(fstate in_fstate, int site, int spin) nogil:
+cdef inline int has_occupation_spin(fstate in_fstate, int site, int spin) nogil:
     if spin:
         return in_fstate.state >> (in_fstate.size - site) & 1
     else:
         return in_fstate.state >> (2*in_fstate.size - site) & 1
-
     
+
 # flip_spin method returns a state with a flipped @spin at @flip_site
 
 cdef fstate flip_spin(fstate in_fstate, int flip_site, int spin) nogil:
@@ -71,10 +77,10 @@ cdef fstate flip_spin(fstate in_fstate, int flip_site, int spin) nogil:
     return result
 
 
-# annahilation operator
+# annihilation operator
 # returns a state and changes the norm of @result
 
-cdef fstate apply_fannahilator_spin(fstate in_fstate, int site, int spin) nogil:
+cdef fstate apply_fannihilator_spin(fstate in_fstate, int site, int spin) nogil:
     cdef fstate result = in_fstate
     if has_occupation_spin(in_fstate, site, spin):
         result = flip_spin(in_fstate, site, spin)
@@ -84,67 +90,66 @@ cdef fstate apply_fannahilator_spin(fstate in_fstate, int site, int spin) nogil:
     return result
 
 
-#tests
-#count_ones_spin test
+# creator operator
+# returns a state
+
+cdef fstate apply_fcreator_spin(fstate in_fstate, int site, int spin) nogil:
+    cdef fstate result = in_fstate
+    if has_occupation_spin(in_fstate, site, spin):
+        result.norm_const = result.norm_const*0
+    else:
+        result = flip_spin(in_fstate, site, spin)
+        result.norm_const = result.norm_const*((-1)**(count_ones_spin(in_fstate, site)))
+    return result
+
+# number operator 
+
+
+# tests
 cdef fstate test
 test.state = 4294967295
 test.size = 16
 test.norm_const = 1
-print("expected value: 30. output:", count_ones_spin(test, 16))
 
-# flip_spin test, spin down
 cdef fstate test1
 test1.state = 172
 test1.size = 4
 test1.norm_const = 1
+
+print("count_ones_spin test:")
+print("expected value: 30. output:", count_ones_spin(test, 16))
+
+
+print("flip_spin test, spin down:")
 print("expected state value: 44. output:", flip_spin(test1, 3, 0))
 
-# flip_spin test, spin up
+print("flip_spin test, spin up:")
 print("expected state value: 164. output:", flip_spin(test1, 1, 1))
 
-#apply_fannahilator_spin test
-cdef fstate test2
-test2.state = 172
-test2.size = 4
-test2.norm_const = 1
 
-#test spin down, occupation 0
-print("expected value: 172. output:", apply_fannahilator_spin(test2, 2, 0))
+print("apply_fannihilator_spin tests")
+# test spin down, occupation 0
+print("expected value: 172. output:", apply_fannihilator_spin(test1, 2, 0))
 
-#test spin up, occupation 0,
-print("expected value: 172. output:", apply_fannahilator_spin(test2, 3, 1))
+# test spin up, occupation 0,
+print("expected value: 172. output:", apply_fannihilator_spin(test1, 3, 1))
 
-#test spin down, occupation 1
-print("expected value: 140. output:", apply_fannahilator_spin(test2, 3, 0))
+# test spin down, occupation 1
+print("expected value: 140. output:", apply_fannihilator_spin(test1, 3, 0))
 
-#test spin up, occupation 1
-print("expected value: 164. output:", apply_fannahilator_spin(test2, 1, 1))
+# test spin up, occupation 1
+print("expected value: 164. output:", apply_fannihilator_spin(test1, 1, 1))
 
-"""
-The 3 basic operators in the spinless code are
-apply_fannahilator_nospin(fstate in_state, int site)
-apply_fcreator_nospin(fstate in_state, int site)
-number_operator(fstate in_state, int site)
 
-these need duplicates for the spin system counterpart
+print("apply_fcreator_spin tests")
+# test spin down, occupation 0
+print("expected value: 172. output:", apply_fcreator_spin(test1, 2, 0))
 
-apply_fannahilator_spin(fstate in_state, int site, char spin)
-apply_fcreator_spin(fstate in_state, int site, char spin)
-number_operator_spin(fstate in_state, int site, char spin)
+# test spin up, occupation 0,
+print("expected value: 174. output:", apply_fcreator_spin(test1, 3, 1))
 
-where char is an 8-bit mem block
-ideally we can use int and represent the spin as 0 or 1 but because these fx
-will be called millions of times you can save 8x more registry memory with temp variables
-another way, because these are meant to be read and not modified
-is to make the input a char* pointer. Passing by reference instead of by value is much faster
+# test spin down, occupation 1
+print("expected value: 172. output:", apply_fcreator_spin(test1, 3, 0))
 
-so you create globals
-cdef char down_representation = 0
-cdef char up_representation = 1
-cdef char* down = &down_representation
-cdef char* up = &up_representation
-
-and then you can for example do has_occupation(fstate=test_state, index=6, spin=down)
-this may need more troubleshooting
-
-"""
+# test spin up, occupation 1
+print("expected value: 172. output:", apply_fcreator_spin(test1, 1, 1))
